d7fb0779bee90e53dac3ea0806ad85f9f4c79bf6
한유경 <yugyeong.han@seculayer.com>
2023-11-20 12:55:47 +0900

    2023-11-20 1) 시큐리티 설정

---
+++ src/main/java/com/example/security1/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example.security1.config;
+
+public class SecurityConfig {
+}
--- src/main/java/com/example/security1/controller/IndexController.java
+++ src/main/java/com/example/security1/controller/IndexController.java
@@ -1,7 +1,9 @@
 package com.example.security1.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //View를 리턴하겠다!!
 public class IndexController {
@@ -14,4 +16,34 @@ public class IndexController {
         //뷰리졸버 설정: templates (prefix), .mustache(suffix)
         return "index"; // src/main/resources/templates/index.mustache
     }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+    @GetMapping("/admin")
+    public @ResponseBody String admin(){
+        return "admin";
+    }
+    @GetMapping("/manager")
+    public @ResponseBody String manager(){
+        return "manager";
+    }
+
+    //스프링시큐리티 해당 주소로 낚아챔
+    @GetMapping("/login")
+    public @ResponseBody String login(){
+        return "login";
+    }
+    @GetMapping("/join")
+    public @ResponseBody String join(){
+        return "join";
+    }
+    @GetMapping("/joinProc")
+    public @ResponseBody
+    String joinProc(){
+        return "회원가입 완료됨!!";
+    }
+
+
 }
